f8266d3608619e824f34ee36c2aa77f63f8e51cf
Joao Victor <joaovictorsrs2002@gmail.com>
2024-06-14 20:11:09 -0300

    Signed-off-by: Joao Victor <joaovictorsrs2002@gmail.com>

---
+++ DiagramaClasseJogo/Level.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.Attribute1 = None
+        self.entity_list = None
+        self.Name = None
+
+    def run(self, ):
+        pass
+
